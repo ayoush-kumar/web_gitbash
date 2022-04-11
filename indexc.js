afead41c1cf3847cbf788ff12dd455a21b58b99a
Ayoush Kumar <ayoushkumar711gmail.com>
2022-04-11 14:38:24 +0530

    first commit

---
+++ indexc.js
@@ -0,0 +1,30 @@
+//     console.log("ola amegos");
+
+    //     consol.warn("hahahahhaha");
+    //     console.error("this is an error u jarck ice of shit can't solve this");
+    //     alert("this is an error u jarck pice of shit can't solve this");
+
+
+
+    var n1 = 12;
+    var n2 = 18;
+
+    console.log(n1 + n2);
+
+    var marks = {
+        raju : 11,
+        piya : 99,
+        rohit : 77.890
+    }
+
+    console.log(marks)
+
+    var arr = [1,2,3,4,5,6,6];
+    
+
+    function avg(a , b)
+    {
+        return (a+b)/2;
+    }
+
+    console.log(avg(99,51));
---
+++ testcase.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"😁😁😁😁😁";
+    return 0;
+}
